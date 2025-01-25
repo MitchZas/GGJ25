78b8f39fe77baf6f0ba3c6217f1ad268b71e3221
MitchZas <mitchellzasa@gmail.com>
2025-01-24 21:47:04 -0500

    Made the Bottom move left and right

--- GGJ25/Assets/BubbleMovement.cs
+++ GGJ25/Assets/BubbleMovement.cs
@@ -4,6 +4,7 @@ public class BubbleMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float downStrength = 5f;
+    public float horizontalStrength = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,14 +15,19 @@ public class BubbleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = Vector2.down * downStrength; 
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
+            rb.linearVelocity = Vector2.left * horizontalStrength;
+        }
 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.linearVelocity = Vector2.right * horizontalStrength;
         }
     }
 }
