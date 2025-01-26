d5ae40f4f65e8caf0ecd68c3de12d5949d383859
MitchZas <mitchellzasa@gmail.com>
2025-01-26 13:39:06 -0500

    Fish Patrol is fixed and Level 2 almost done

--- GGJ25/Assets/Scripts/FishPatrol.cs
+++ GGJ25/Assets/Scripts/FishPatrol.cs
@@ -1,28 +1,54 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class FishPatrol : MonoBehaviour
 {
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+
+    private Transform currentPoint;
+
     public float speed;
-    public float rayDist;
-    private bool movingRight;
-    public Transform groundDetect;
+
+    [SerializeField] SpriteRenderer fishSprite;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+    }
+
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundCheck = Physics2D.Raycast(groundDetect.position,Vector2.right,rayDist);
-
-        if (groundCheck.collider == true)
+        Vector2 point = currentPoint.position - transform.position;
+        
+        if(currentPoint == pointB.transform)
         {
-            if (movingRight == true)
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
-            }
+            rb.linearVelocity = new Vector2(speed, 0);
+            fishSprite.flipX = false;
         }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+            fishSprite.flipX = true;
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position,pointB.transform.position);
     }
 }
